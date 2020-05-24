47d2422c68dc3d11a7a21ac7ff2c4aca718a4de9
kimcasamina <kim.csmn@gmail.com>
2020-05-24 23:33:25 +0800

    minor changes

--- app/js/accordion.js
+++ app/js/accordion.js
@@ -1,14 +1,11 @@
 const acc = document.querySelector(".faq-body");
 
 acc.addEventListener("click", function (e) {
-  // console.log(e.target);
-
   e.preventDefault();
   const accBtn = e.target;
   accBtn.classList.toggle("active");
   const arrow = e.target.children[0];
   arrow.classList.toggle("reverse");
-  console.log(e.target);
 
   const panel = e.target.nextElementSibling;
   if (panel.style.display === "block") {
